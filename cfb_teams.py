a52657aca388749ec57b3e10032dba732602d8aa
thedataguy239 <thedataguy239@gmail.com>
2025-08-29 16:53:40 -0400

    Create cfb_teams.py

---
+++ cfb_teams.py
@@ -0,0 +1,17 @@
+POWER_FIVE_TEAMS = {
+    # ACC
+    "BC","CLEM","DUKE","FSU","GT","LOU","MIA","UNC","NCST",
+    "PITT","SYR","UVA","VT","WAKE","ND",
+    # Big Ten
+    "ILL","IND","IOWA","MD","MICH","MSU","MINN","NEB","NW",
+    "OHST","PSU","PUR","RUTG","WISC",
+    # Big 12
+    "BAYL","ISU","KU","KSU","OKLA","OKST","TCU","TEX","TTU",
+    "WVU","BYU","CINC","HOU","UCF",
+    # Pac-12
+    "ARIZ","ASU","CAL","COLO","ORE","ORST","STAN","UCLA","USC",
+    "UTAH","WASH","WSU",
+    # SEC
+    "ALA","ARK","AUB","FLA","UGA","UK","LSU","MSST","MIZ","MISS",
+    "SC","TENN","TAMU","VAND"
+}
